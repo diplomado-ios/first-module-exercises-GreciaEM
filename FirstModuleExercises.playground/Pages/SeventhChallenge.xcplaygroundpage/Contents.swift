0b0562339093bb2333b4d11cf571377b40682f34
Alejandro Mendoza <zippi180@gmail.com>
2023-09-09 01:34:20 -0600

    Update base playground code

--- FirstModuleExercises.playground/Pages/SeventhChallenge.xcplaygroundpage/Contents.swift
+++ FirstModuleExercises.playground/Pages/SeventhChallenge.xcplaygroundpage/Contents.swift
@@ -1,7 +1,24 @@
-//: [Previous](@previous)
-
 import Foundation
+import XCTest
 
-var greeting = "Hello, playground"
+class SieveTest: XCTestCase {
 
-//: [Next](@next)
+    func testPrimes() {
+        // given
+        let firstTestInput = 10
+        let secondTestInput = 50
+        let thirdTestInput = 85
+
+        // when
+        let firstPrimeNumbers = PrimeCalculator.calculate(upTo: firstTestInput)
+        let secondPrimeNumbers = PrimeCalculator.calculate(upTo: secondTestInput)
+        let thirdPrimeNumbers = PrimeCalculator.calculate(upTo: thirdTestInput)
+
+        // then
+        XCTAssertEqual(firstPrimeNumbers.count, 4)
+        XCTAssertEqual(secondPrimeNumbers.count, 15)
+        XCTAssertEqual(thirdPrimeNumbers.count, 23)
+    }
+}
+
+SieveTest.defaultTestSuite.run()

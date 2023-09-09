d9ceb2f85202ba897d022516a64327b232ae595c
Alejandro Mendoza <zippi180@gmail.com>
2023-09-09 01:35:10 -0600

    Update exercise order

--- FirstModuleExercises.playground/Pages/EightChallenge.xcplaygroundpage/Contents.swift
+++ FirstModuleExercises.playground/Pages/EightChallenge.xcplaygroundpage/Contents.swift
@@ -1,2 +1,24 @@
 import Foundation
+import XCTest
 
+class SieveTest: XCTestCase {
+
+    func testPrimes() {
+        // given
+        let firstTestInput = 10
+        let secondTestInput = 50
+        let thirdTestInput = 85
+
+        // when
+        let firstPrimeNumbers = PrimeCalculator.calculate(upTo: firstTestInput)
+        let secondPrimeNumbers = PrimeCalculator.calculate(upTo: secondTestInput)
+        let thirdPrimeNumbers = PrimeCalculator.calculate(upTo: thirdTestInput)
+
+        // then
+        XCTAssertEqual(firstPrimeNumbers.count, 4)
+        XCTAssertEqual(secondPrimeNumbers.count, 15)
+        XCTAssertEqual(thirdPrimeNumbers.count, 23)
+    }
+}
+
+SieveTest.defaultTestSuite.run()
